c1a6f73a5f028548f789d961343a118436677242
Josh Gallant <114911226+jgallantgs@users.noreply.github.com>
2023-03-05 01:18:29 +0000

    Update bitrise-cli.rb

--- bitrise-cli.rb
+++ bitrise-cli.rb
@@ -2,7 +2,7 @@ class BitriseCli < Formula
   desc "A command-line interface for Bitrise"
   homepage "https://github.com/jgallantgs/bitrise-cli"
   url "https://raw.githubusercontent.com/jgallantgs/bitrise-cli/main/bitrise-cli.zsh"
-  sha256 "e1d067c346f76b6f35c6d2ed6c194e6e7d6c3d1f3e9d196b8a7b53116bce1cb3"
+  sha256 "5234c5d54a6a3b8c63fa19989fcf364904835e507c742b830aaf1dafc8eee47c"
 
   def install
     bin.install "bitrise-cli.sh" => "bitrise"
